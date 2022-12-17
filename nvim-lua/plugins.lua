147dc8389220825f42e113f7938f8ee2c56cb45d
sudo <meshackmasam@gmail.com>
2022-12-18 02:20:44 +0800

    vi -> nvim

---
+++ nvim-lua/plugins.lua
@@ -0,0 +1,33 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  -- plugin manager
+  use 'wbthomason/packer.nvim'
+  -- theme 
+  use 'EdenEast/nightfox.nvim' 
+  -- git utils 
+  use 'airblade/vim-gitgutter'
+  use 'tpope/vim-fugitive'
+  -- visual
+  use 'yggdroot/indentline'
+  -- tmux
+  use 'edkolev/tmuxline.vim'
+  use 'christoomey/vim-tmux-navigator'
+  -- cursor manager
+  use 'terryma/vim-multiple-cursors'
+  -- editor specific config loader .editorconfig
+  use 'editorconfig/editorconfig-vim'
+  -- buffer top line
+  use 'ap/vim-buftabline'
+
+  use { 'junegunn/fzf', run = function() vim.fn['fzf#install']() end }
+  use 'junegunn/fzf.vim'
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  use 'dstein64/vim-startuptime'
+
+
+end)
