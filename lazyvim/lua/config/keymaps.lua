bc400cf1b762bde50ec4305b63d45c301aaf8dd9
sudo <meshackmasam@gmail.com>
2024-03-07 13:09:41 +0300

    development

--- lazyvim/lua/config/keymaps.lua
+++ lazyvim/lua/config/keymaps.lua
@@ -26,7 +26,8 @@ map("n", "<space>gs", "<cmd>:Git<CR>")
 map("n", "<space>gc", "<cmd>:Git commit -v -q<CR>")
 map("n", "<space>gd", "<cmd>:Gdiff<CR>")
 
-map("n", "<C-o>", "<cmd>:NvimTreeOpen<CR>")
+-- map("n", "<C-o>", "<cmd>:NvimTreeOpen<CR>")
+map("n", "<C-o>", "<cmd>:NvimTreeFindFile<CR>")
 
 -- local opts = { noremap = true, silent = true }
 -- map("n", "<C-j>", "<Down>", opts)
---
+++ lazyvim/lua/plugins/leap.lua
@@ -0,0 +1,23 @@
+return {
+  "ggandor/leap.nvim",
+  enabled = false,
+  opts = {
+    case_sensitive = false,
+    equivalence_classes = { " \t\r\n" },
+    max_phase_one_targets = nil,
+    highlight_unlabeled_phase_one_targets = false,
+    max_highlighted_traversal_targets = 10,
+    substitute_chars = {},
+    safe_labels = "sfnut/SFNLHMUGTZ?",
+    labels = "sfnjklhodweimbuyvrgtaqpcxz/SFNJKLHODWEIMBUYVRGTAQPCXZ?",
+    special_keys = {
+      next_target = "<enter>",
+      prev_target = "<tab>",
+      next_group = "<space>",
+      prev_group = "<tab>",
+    },
+  },
+  config = function()
+    require("leap").add_default_mappings()
+  end,
+}
